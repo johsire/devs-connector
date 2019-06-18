d5eb04e38e311bea44e2f6100056208d0de3001c
dev_johsire <josiah5115@gmail.com>
2019-06-18 12:27:05 -0600

    server modified & config directory

---
+++ config/db.js
@@ -0,0 +1,23 @@
+// Connect to the database
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+
+        console.log('MongoDB Connection established...');
+    } catch(err) {
+        console.log(err.message);
+
+        //Exit process with failure
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+//server
 const express = require('express');
 
+const connectDB = require('./config/db');
+
 const app = express();
+
+// Connect the Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
