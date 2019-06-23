a0d972459b0d771529e9a0edd1c7bfa9e1ef0402
dev_johsire <josiah5115@gmail.com>
2019-06-23 01:57:29 -0600

    Delete Profile Experience Final

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -124,8 +124,9 @@ router.get('/', async (req, res) => {
 // @access Public
 router.get('/user/:user_id', async (req, res) => {
   try {
-    const profile = await Profile.findOne({ user: req.params.user_id }).populate('user',
-      ['name', 'avatar']);
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+      }).populate('user', ['name', 'avatar']);
 
     // Check if a Profile for the User Exists
     if (!profile) return res.status(400).json({ msg: 'Profile not found!' });
@@ -218,5 +219,28 @@ router.put(
       res.status(500).send('Server Error');
     }
   });
+
+  // @route    DELETE api/profile/experience/:exp_id
+  // @desc     Delete Experience from Profile
+  // @access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get the Remove Index
+    const removeIndex = profile.experience.map(item => item.id).indexOf
+      (req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error!');
+  }
+});
 
 module.exports = router;
