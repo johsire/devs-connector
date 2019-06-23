9133902413d8eab91b11356265f52b35e33fe92b
dev_johsire <josiah5115@gmail.com>
2019-06-23 08:32:10 -0600

    Get & Delete Post Routes Initial

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -16,12 +16,12 @@ const Profile = require('../../models/Profile');
 
 
 
-//---------------ADD USER POST START---------------\\
+//-----------------USER POST START-----------------\\
 
 // @route  POST api/posts
 // @desc   Create a Post
 // @access Private
-router.post("/", [auth, [
+router.post('/', [auth, [
     check('text', 'Text is required!')
     .not()
     .isEmpty()
@@ -33,7 +33,7 @@ router.post("/", [auth, [
       return res.status(400).json({
         errors: errors.array()
       });
-    }
+    };
 
     try {
       const user = await User.findById(req.user.id).select('-password');
@@ -55,7 +55,44 @@ router.post("/", [auth, [
     }
   });
 
-//---------------ADD USER POST END---------------\\
+// @route    GET api/posts
+// @desc     Get all posts
+// @access   Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({
+      date: -1
+    });
 
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error!');
+  }
+});
+
+// @route    GET api/posts/:id
+// @desc     Get posts by ID
+// @access   Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found!' });
+    };
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found!' });
+    }
+    res.status(500).send('Server Error!');
+  }
+});
+
+//-------------------USER POST END-------------------\\
 
 module.exports = router;
