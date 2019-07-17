6d370a8918018004af4b2cf0e8ac72f2102e1302
dev_johsire <josiah5115@gmail.com>
2019-07-17 16:51:43 -0600

    Profile Reducer & Get Current Profile Initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch }
+  from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
@@ -48,6 +49,7 @@ const App = () => {
         </Fragment>
       </Router>
     </Provider>
-  );};
+  );
+};
 
 export default App;
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -7,3 +7,5 @@ export const USER_LOADED = "USER_LOADED";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_FAIL = "LOGIN_FAIL";
 export const LOGOUT = "LOGOUT";
+export const GET_PROFILE = "GET_PROFILE";
+export const PROFILE_ERROR = "PROFILE_ERROR";
