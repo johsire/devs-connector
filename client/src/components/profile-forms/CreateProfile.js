ed3657db011777e51ef9e49a6b6e6ab69d59e503
dev_johsire <josiah5115@gmail.com>
2019-07-18 02:02:20 -0600

    Edit Profile Comp Bug

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -212,9 +212,9 @@ const CreateProfile = ({ createProfile, history }) => {
         )}
 
         <input type="submit" className="btn btn-primary my-1" />
-        <a className="btn btn-light my-1" href="dashboard.html">
+        <Link className="btn btn-light my-1" href="dashboard.html">
           Go Back
-        </a>
+        </Link>
       </form>
     </Fragment>
   );
