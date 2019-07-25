ce927a6e6c435a72e6b0bdf352cbc630580d2049
dev_johsire <josiah5115@gmail.com>
2019-07-24 22:22:04 -0600

    Post Item Comp Initial

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,19 +3,30 @@ import PropTypes from "prop-types";
 import React, { Fragment, useEffect } from "react";
 
 import { getPosts } from "../../actions/post";
+import PostItem from "./PostItem";
 import Spinner from "../layout/Spinner";
 
-const Posts = ({
-  getPosts,
-  post: {
-    posts,
-    loading
-  } }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
-  }, [getPosts])
+  }, [getPosts]);
 
-  return <div></div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fast fas-user" /> Welcome to the community
+      </p>
+      {/* PostForm */}
+      <div className="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </Fragment>
+  );
 };
 
 Posts.propTypes = {
