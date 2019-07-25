e38071fee377cb83973082b90f38f84be5f12728
dev_johsire <josiah5115@gmail.com>
2019-07-25 02:34:05 -0600

    Single Post Display Final

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,8 +1,10 @@
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import React, { Fragment, useEffect } from "react";
 
 import { getPost } from "../../actions/post";
+import PostItem from "../posts/PostItem";
 import Spinner from "../layout/Spinner";
 
 const Post = ({ getPost, match, post: { post, loading } }) => {
@@ -10,7 +12,16 @@ const Post = ({ getPost, match, post: { post, loading } }) => {
     getPost(match.params.id);
   }, [getPost, match.params.id]);
 
-  return <div>post</div>;
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <Fragment>
+      <Link to="/posts" className="btn">
+        Back to Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </Fragment>
+  );
 };
 
 Post.propTypes = {
