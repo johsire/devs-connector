434457ccaf0334be1176be6a0a8cbb181edcd4b2
dev_johsire <josiah5115@gmail.com>
2019-07-29 16:08:56 -0600

    Prepared for Deployment

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use("/api/posts", require("./routes/api/posts"));
 // Serve Static Assets in Production
 if (process.env.NODE_ENV === "production") {
   // Set Static Folder
-  app.use(express.static(client / build));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
