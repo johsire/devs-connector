5d2913eb6577374c1dd73ed9879b2c904337579c
dev_johsire <josiah5115@gmail.com>
2019-07-29 15:53:32 -0600

    Prepate for Deploy Final

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // server
 const express = require("express");
+const path = require("path");
 
 const connectDB = require("./config/db");
 
@@ -11,13 +12,21 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("API Running"));
-
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve Static Assets in Production
+if (process.env.NODE_ENV === "production") {
+  // Set Static Folder
+  app.use(express.static(client / build));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
